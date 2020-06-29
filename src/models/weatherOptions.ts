59d5e85ba167cd2e3f026bda383236185a7fe1b7
junolee <junolee7803@gmail.com>
2020-06-30 01:18:18 +0900

    add models/weatherOptions

---
+++ src/models/weatherOptions.ts
@@ -0,0 +1,59 @@
+  // models로 옮기자
+  export const weatherOptions = {
+    Rain: {
+      colors: ["#00C6FB", "#005BEA"],
+      title: "Raining",
+      subtitle: "우산 챙기세요",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/rain.PNG",
+    },
+    Clear: {
+      colors: ["#FEF253", "#FF7300"],
+      title: "Sunny",
+      subtitle: "날이 좋아요",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/clear.PNG",
+    },
+    Thunderstorm: {
+      colors: ["#00ECBC", "#007ADF"],
+      title: "Thunderstrom",
+      subtitle: "천둥이쳐요!",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/thunderstrom.PNG",
+    },
+    Clouds: {
+      colors: ["#D7D2CC", "#304352"],
+      title: "Clouds",
+      subtitle: "구름이 꼈어요",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/clouds.PNG",
+    },
+    Snow: {
+      colors: ["#7DE2FC", "#B9B6E5"],
+      title: "Snow",
+      subtitle: "눈이와요!",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/snow.PNG",
+    },
+    Drizzle: {
+      colors: ["#89F7FE", "#66A6FF"],
+      title: "Drizzle",
+      subtitle: "이슬비가 내려요",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/drizzle.PNG",
+    },
+    Haze: {
+      colors: ["#89F7FE", "#66A6FF"],
+      title: "Haze",
+      subtitle: "안개가 꼈어요",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/haze.PNG",
+    },
+    Mist: {
+      colors: ["#89F7FE", "#66A6FF"],
+      title: "Mist",
+      subtitle: "안개가 꼈어요",
+      icon:
+        "https://weather-fit.s3.ap-northeast-2.amazonaws.com/weather/mist.PNG",
+    },
+  };
